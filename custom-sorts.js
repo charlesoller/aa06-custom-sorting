7b517885f81e1fc21295ffd1667ec8501c94087f
charlesoller <charlesrello@gmail.com>
2023-11-06 18:40:29 -0500

    3 done

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,21 +1,36 @@
 function ageSort(users) {
-  // Your code here 
+  // Your code here
+
+  return users.sort((a, b) => a.age - b.age);
 }
 
+// function compareNumbers(a, b) {
+//   return a - b;
+// }
+
 function oddEvenSort(arr) {
-  // Your code here 
+  return arr.sort((a,b) => {
+    let isAEven = a % 2 === 0;
+    let isBEven = b % 2 === 0;
+
+    if(isAEven && !isBEven) return 1; //positive value means that b is sorted ahead of a
+    if(!isAEven && isBEven) return -1; //negative value means that a stays to the left of b
+    return a - b;
+  })
 }
 
 function validAnagrams(s, t) {
-  // Your code here 
+  t = t.split('').sort().join('')
+  s = s.split('').sort().join('')
+  return t === s;
 }
 
 function reverseBaseSort(arr) {
-  // Your code here 
+  // Your code here
 }
 
 function frequencySort(arr) {
-  // Your code here 
+  // Your code here
 }
 
 module.exports = [
